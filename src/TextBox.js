de3226a958f15196a9f79c89e710c1473a6a0eed
Kavish Rizvi <rizvikavish205.com>
2022-08-14 00:04:15 +0530

    Added Button Func

--- src/TextBox.js
+++ src/TextBox.js
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
 function TextBox(){
-    const [data, setData]=useState(null)
+    const [data, setData]=useState(null);
+    const [print, setPrint]=useState(false);
     function getData(val){
         setData(val.target.value)
         console.warn(val.target)
     }
     return(
         <div className="App" align="center">
+          {print?
           <h1>{data}</h1>
+          :null
+          }
           <label>User Name</label>
           <input type="text" onChange={getData}/>
           <br/>
           <label>Password</label>
           <input type="text" onChange={getData}/>
           <br/>
-          <button>Login</button>
+          <button onClick={()=>setPrint(true)}>Login</button>
           <button>Clear</button>
         </div>
       );
